12511318bf5bf09f4b7466afc66fc473bd71a611
nicopressez <154272808+nicopressez@users.noreply.github.com>
2024-01-28 15:40:10 +0100

    Added add message form and message render

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Bonjour",
+    user: "Jean",
+    added: new Date()
+  },
+  {
+    text: "Hola",
+    user: "Juan",
+    added: new Date()
+  },
+  {
+    text: "Hello",
+    user: "John",
+    added: new Date()
+  },
+]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Messages', messages: messages });
 });
+
+router.get('/new', (req,res,next) => {
+  res.render('form')
+})
+
+router.post('/new', (req,res,next) => {
+  const user = req.body.name;
+  const message = req.body.message;
+  messages.push({text: message, user: user,added: new Date() });
+  res.redirect('/');
+})
 
 module.exports = router;
